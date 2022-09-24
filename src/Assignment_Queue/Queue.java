4b6dd3911a1e73534d8ef6e6ec2ce2d32dcffc43
Muadali <108030259+Muadali@users.noreply.github.com>
2022-09-24 13:00:16 -0700

    Update Queue.java

--- src/Assignment_Queue/Queue.java
+++ src/Assignment_Queue/Queue.java
@@ -2,34 +2,32 @@ package Assignment_Queue;
 
 import java.util.NoSuchElementException;
 
-public class Queue<T> {
+public class Queue<T> extends GenericLinkedList<T>{
 		 
 	    private GenericLinkedList<T> queue;
 	 
 	    public Queue() {
-	    	queue=new GenericLinkedList<>();
+	    	super();
 	      //  queue = new GenericLinkedList<>();
 	    }
 	 
-	    public void enqueue(T data ) {
-	    	queue.addNode(new GenericNode<>());
+	    public void enqueue(GenericNode<T> node ) {
+	    	super.addNode(Node);
 	        //queue.addNode(new GenericNode<>(data));
 	    }
+	
+	public GenericNode < T > dequeue ( ) {
+		if (this.isEmpty ( ) ){
+			return null ;
+		}
+		
+        	GenericNode < T > front = head ;
+        	head head.getNextNode ( ) ;
+        	this.length-- ;
+        	return front ;
+} // end dequeue
 	 
-	    public T dequeue() {
-	        if (queue.isEmpty()) {
-	            throw new NoSuchElementException("Can not dequeue from an empty queue.");
-	        }
-	 
-	        T data = queue.getList().getData();
-	       // queue.setList(queue.getList().getNextNode());
-	 
-	        return data;
-	    }
-	 
-	    public boolean isEmpty() {
-	        return queue.isEmpty();
-	    }
+	  
 	 
 	   
 
