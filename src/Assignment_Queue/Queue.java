4ead4d991fdaffd20e9907cbd30e632e73e9303b
Muadali <108030259+Muadali@users.noreply.github.com>
2022-09-26 22:47:45 -0700

    Update Queue.java

--- src/Assignment_Queue/Queue.java
+++ src/Assignment_Queue/Queue.java
@@ -1,34 +1,29 @@
 package Assignment_Queue;
 
-import java.util.NoSuchElementException;
-
 public class Queue<T> extends GenericLinkedList<T>{
-		 
-	    private GenericLinkedList<T> queue;
 	 
-	    public Queue() {
-	    	super();
-	      //  queue = new GenericLinkedList<>();
-	    }
-	 
-	    public void enqueue(GenericNode<T> node ) {
-	    	super.addNode(Node);
-	        //queue.addNode(new GenericNode<>(data));
-	    }
-	
-	public GenericNode < T > dequeue ( ) {
-		if (this.isEmpty ( ) ){
-			return null ;
-		}
-		
-        	GenericNode < T > front = head ;
-        	head=head.getNextNode ( ) ;
-        	this.length-- ;
-        	return front ;
-} // end dequeue
-	 
-	  
-	 
-	   
+    //private GenericLinkedList<T> queue;
+ 
+    public Queue() {
+    	super();
+      //  queue = new GenericLinkedList<>();
+    }
+ 
+    public void enqueue(GenericNode<T> node ) {
+    	super.addNode(node);
+        //queue.addNode(new GenericNode<>(data));
+    }
 
-}
+    public GenericNode <T> dequeue ( ) {
+    	if (this.isEmpty ( ) ){
+		return null ;
+		}
+	
+    	GenericNode < T > front = head ;
+    	head=head.getNextNode ( ) ;
+    	this.length-- ;
+    	return front ;
+    } // end dequeue
+
+
+}//end class
